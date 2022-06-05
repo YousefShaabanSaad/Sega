157e64a34c3ccbfbf60739501a4d6800cd225c3c
YousefShaabanSaad <101521680+YousefShaabanSaad@users.noreply.github.com>
2022-06-05 17:52:05 +0200

    update HomeActivity

--- app/src/main/java/com/yousef/sega/activity/HomeActivity.java
+++ app/src/main/java/com/yousef/sega/activity/HomeActivity.java
@@ -6,6 +6,8 @@ import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.yousef.sega.R;
+import com.yousef.sega.databinding.ActivityHomeBinding;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -13,8 +15,6 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.yousef.sega.activity.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -40,8 +40,7 @@ public class HomeActivity extends AppCompatActivity {
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
---
+++ app/src/main/java/com/yousef/sega/fragment/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.yousef.sega.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.yousef.sega.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
