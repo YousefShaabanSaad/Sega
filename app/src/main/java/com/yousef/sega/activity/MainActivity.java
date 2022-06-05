a906364cf0a19279168d3a7d88ee5076fbf282a7
YousefShaabanSaad <101521680+YousefShaabanSaad@users.noreply.github.com>
2022-06-05 17:45:54 +0200

    update HomeActivity

--- app/src/main/java/com/yousef/sega/activity/MainActivity.java
+++ app/src/main/java/com/yousef/sega/activity/MainActivity.java
@@ -2,15 +2,45 @@ package com.yousef.sega.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import com.yousef.sega.R;
+import com.yousef.sega.database.Repository;
 
 public class MainActivity extends AppCompatActivity {
+
+    //  SHA1 and SHA-256
+    //  Valid until:
+    //  TODO Sunday, March 31, 2052
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(1000);
+                    checkUser();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    private void checkUser() {
+        Repository repository=new Repository();
+        Intent intent = new Intent();
+
+        if(repository.getUser() != null){
+            intent.setClass(this, HomeActivity.class);
+        }else
+            intent.setClass(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
