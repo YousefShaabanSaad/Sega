7ff10e591dc044c15f24610ff926e306309ec38c
YousefShaabanSaad <101521680+YousefShaabanSaad@users.noreply.github.com>
2022-06-06 20:05:15 +0200

    update ProfileActivity

--- app/src/main/java/com/yousef/sega/activity/ProfileActivity.java
+++ app/src/main/java/com/yousef/sega/activity/ProfileActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.yousef.sega.databinding.ActivityProfileBinding;
+
 public class ProfileActivity extends AppCompatActivity {
 
+    private ActivityProfileBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
